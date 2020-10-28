5a79fbc790129dc07bc49edf2da1d22f86a8f7a4
Bhajan <56666353+pooniab@users.noreply.github.com>
2020-10-28 06:58:22 +0530

    Add files via upload

--- src/main/java/array/maxDiffIndex.java
+++ src/main/java/array/maxDiffIndex.java
@@ -6,12 +6,36 @@ public class maxDiffIndex {
 		// TODO Auto-generated method stub
 		
 	int arr[] = {8,10,32,52,9,80,7};
-	int current_max =0;
-    int max=0;
+	int len = arr.length;
+	
     
-    //max=
+    System.out.println("maximum differenc is :" + findMax(arr,len));
+
+    
     
 
+	}
+
+	private static int findMax(int[] arr, int len) {
+		
+	    int current_max=arr[1]-arr[0];
+	    int min_element = arr[0];
+	    for (int i = 1; i < arr.length; i++) {
+	    	
+	    	int max=arr[i]-min_element;
+	    	
+	    	if (max > current_max)
+	    	{
+	    		current_max=max;
+	    	}
+			if (arr[i]<min_element) {
+				min_element=arr[i];
+			}
+		}
+	    
+	    
+		// TODO Auto-generated method stub
+		return current_max;
 	}
 
 }
